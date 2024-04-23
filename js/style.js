d43e86a4219dbd08a66ac3ee12730166521bfdca
XAnthekiraX <alexbalverde123@gmail.com>
2024-04-22 21:54:02 -0500

    Updated

--- js/style.js
+++ js/style.js
@@ -1,11 +1,18 @@
 // Animación del fondo de pantalla
-var spans = document.querySelectorAll('.bubble');
-
+var spans = document.querySelectorAll('.bubble')
 spans.forEach(function(span) {
     var randomNumber = Math.floor(Math.random() * (30 - 10 + 1)) + 10;
     
     span.style.setProperty('--i', randomNumber);
 });
+
+var nav = document.querySelectorAll('.nav-opt');
+
+// Iterar sobre cada elemento .line
+for (var i = 0; i < nav.length; i++) {
+    // Agregar una clase dinámica con el índice aumentado en 1
+    nav[i].classList.add('opt' + (i + 1));
+}
 
 //boton para cambiar de banco a negro
 $('.black-light').bind('click', function() {
@@ -18,17 +25,75 @@ $('.black-light').bind('click', function() {
     $('.nav-opt').toggleClass('active');
     $('.text').toggleClass('active');
     $('.present2').toggleClass('active');
+    $('#inicio').toggleClass('active');
+    $('#sobre-mi').toggleClass('active');
+    $('#proyectos').toggleClass('active');
+    $('#contacto').toggleClass('active');
+    $('.about').toggleClass('active');
 });
 
 //boton de empezar
 $('.enter').bind('click', function(){
     $('.present').addClass('active');
-    $('#inicio').addClass('active');
-    $('#sobre-mi').addClass('active');
-    $('#proyectos').addClass('active');
-    $('#contacto').addClass('active');
+    $('.present2').addClass('active2');
 });
 
-$('.enter').on('mousedown', function(){
+$('.enter').on('mouseenter', function(){
     $('.enter').addClass('active');
+});
+
+$('.enter').on('mouseleave', function(){
+    $('.enter').removeClass('active');
+});
+
+$('.nav-opt').on('click', function() {
+    // Remover la clase 'select' de todos los elementos
+    $('.nav-opt').removeClass('select');
+    // Añadir la clase 'select' solo al elemento clickeado
+    $(this).addClass('select');
+});
+
+
+// Asigna un manejador de eventos a cada elemento y actualiza las clases según el elemento activo
+$('#inicio, #sobre-mi, #proyectos, #contacto').on('mouseenter', function() {
+    // Remueve la clase 'select' de todos los elementos
+    $('.opt1, .opt2, .opt3, .opt4').removeClass('select');
+    
+    // Añade la clase 'select' basado en el ID del elemento que disparó el evento
+    if (this.id === 'inicio') {
+        $('.opt1').addClass('select');
+    } else if (this.id === 'sobre-mi') {
+        $('.opt2').addClass('select');
+    } else if (this.id === 'proyectos') {
+        $('.opt3').addClass('select');
+    } else if (this.id === 'contacto') {
+        $('.opt4').addClass('select');
+    }
+});
+
+$('.about').on('click', function() {
+    $('.about').toggleClass('press');
+});
+
+
+//Enfoque de cabecera
+// Selecciona todos los enlaces internos
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    // Agrega un evento de clic a cada enlace interno
+    anchor.addEventListener('click', function (e) {
+        // Evita el comportamiento predeterminado de la navegación
+        e.preventDefault();
+        
+        // Obtiene el destino del enlace interno
+        const target = document.querySelector(this.getAttribute('href'));
+
+        // Calcula la posición del destino relativa a la parte superior del documento
+        const offsetTop = target.offsetTop;
+
+        // Desplaza la página para que el destino esté enfocado en la parte superior
+        window.scrollTo({
+            top: offsetTop,
+            behavior: 'smooth' // Agrega un desplazamiento suave
+        });
+    });
 });
